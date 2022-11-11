fd4e4ed115de47c8f1d15bf9ac92b9b5df1d3cee
ishaan8282 <mehtaishan8282@gmail.com>
2022-11-11 10:03:55 +0530

    day3

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignupEmail from './Components/SignupEmail';
 import SignupPassword from './Components/SignupPassword';
-
+import Dashboard  from "./Pages/Dashboard"
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/signup/email" element={<SignupEmail />} />
                 <Route path="/signup/password" element={<SignupPassword />} />
+               
             </Routes>
+             {/* <Dashboard/> */}
         </div>
     );
 }
