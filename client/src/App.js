33c0df38d6d33d3e08d8e8155d4df069e022cffd
Ace0012 <ayushchandra042003@gmail.com>
2022-11-12 17:58:36 +0530

    day-4

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Login from './Components/Login';
 import SignupEmail from './Components/SignupEmail';
 import SignupPassword from './Components/SignupPassword';
-<<<<<<< HEAD
+
 import Homepage from './Pages/Homepage';
 import AllRoutes from './Routes/AllRoutes';
-=======
+
 // import Dashboard  from "./Pages/Dashboard"
 import RequireEmail from './Hoc/RequireEmail';
 import CreateSurvey from "./Components/CreateSurvey";
->>>>>>> 1607eb8460db0dc14ce9390839a5ceee80d408eb
+
 
 function App() {
     return (
@@ -20,10 +20,6 @@ function App() {
             {/* <Homepage/> */}
             {/* <AllRoutes/> */}
             <Routes>
-<<<<<<< HEAD
-               
-
-=======
                 <Route path="/signup/email" element={<SignupEmail />} />
                 <Route
                     path="/signup/password"
@@ -34,7 +30,6 @@ function App() {
                     }
                 />
                 <Route path="/login" element={<Login />} />
->>>>>>> 1607eb8460db0dc14ce9390839a5ceee80d408eb
             </Routes>
             {/* <Dashboard/> */}
             <CreateSurvey />
--- client/src/Routes/AllRoutes.jsx
+++ client/src/Routes/AllRoutes.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
-import Login from './../Components/Login';
+// import Login from './../Components/Login';
 import Homepage from './../Pages/Homepage';
-import SignupEmail from './../Components/SignupEmail';
-import SignupPassword from "../Components/SignupPassword";
+// import SignupEmail from './../Components/SignupEmail';
+// import SignupPassword from "../Components/SignupPassword";
 
 const AllRoutes = () => {
 	return <div>
 		<Routes>
-			<Route path= "/login" element={<Login/>}/>
+		
 			<Route path="/" element={<Homepage/>}/>
-                <Route path="/signup/email" element={<SignupEmail />} />
-                <Route path="/signup/password" element={<SignupPassword  />} />
+
 		</Routes>
 	</div>;
 };
